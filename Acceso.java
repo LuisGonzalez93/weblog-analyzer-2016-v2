05d1837876f4367189a2402fe513af4ca464d69d
LuisGonzalez93 <lg02393@gmail.com>
2017-03-03 14:16:33 +0100

    Comit 01

--- Acceso.java
+++ Acceso.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Acceso
 {
     private int ano;
@@ -5,36 +7,43 @@ public class Acceso
     private int dia;
     private int hora;
     private int minutos;
-    
-    public Acceso(int ano, int mes, int dia, int hora, int minutos)
+
+    public Acceso(String fecha)
     {
-        this.ano = ano;
-        this.mes = mes;
-        this.dia = dia;
-        this.hora = hora;
-        this.minutos = minutos;
+        String[] string = fecha.split(" ");
+        int[] campoFecha = new int[string.length];
+        for (int i = 0 ; i < string.length; i++){
+            campoFecha[i] = Integer.parseInt(string[i]);
+        }
+        this.ano = campoFecha[0];
+        this.mes = campoFecha[1];
+        this.dia = campoFecha[2];
+        this.hora = campoFecha[3];
+        this.minutos = campoFecha[4];
+
     }
-    
+   
+
     public int getAno() 
     {
         return ano;
     }
-    
+
     public int getMes()
     {
         return mes;
     }
-    
+
     public int getDia()
     {
         return dia;
     }
-    
+
     public int getHora()
     {
         return hora;
     }
-    
+
     public int getMinutos()
     {
         return minutos;
